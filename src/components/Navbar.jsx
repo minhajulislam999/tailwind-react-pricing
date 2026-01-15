94b14bbcda3f2752b4ce16940d91d6d508b6ce75
minhajul islam <mm01882390860@gmail.com>
2026-01-16 03:29:44 +0600

    Add responsive navbar with mobile toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Menu } from 'lucide-react';
 import { Chromium } from 'lucide-react';
+import { useState } from 'react';
 
 
 
 function Navbar() {
+
+    const [open, setOpen] =useState(true);
+
   return (
     <nav className='px-4 py-3 bg-gray-900 text-white'>
         <div className='max-w-7xl mx-auto flex justify-between items-center'>
@@ -23,11 +27,24 @@ function Navbar() {
 
             {/* Mobile Button */}
 
-            <button>
+            <button
+            className='md:hidden text-2xl'
+            onClick={() => setOpen(!open)}>
                 <Menu />
             </button>
       
     </div>
+
+    { open &&(
+        <ul className='md:hidden mt-3 space-y-3 bg-gray-800 p-4 rounded'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+        </ul>
+            
+        )
+    }
 
     </nav>
     
